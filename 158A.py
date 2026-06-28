b831a84dfa39829cc6a1af060b972c102a630a9d
pandey-hariom-dev <pandey.hariom.dev@gmail.com>
2026-06-28 15:07:59 +0000

    Jun 28, 2026, 8:37 PM

--- 158A.py
+++ 158A.py
@@ -3,13 +3,8 @@ c = int(input())
 
 a = []
 b = []
-for z in range(0, n):
-    row_a = [int(x) for x in input(f"Enter space-separated elements for a{z}: ").split()]
-    row_b = [int(x) for x in input(f"Enter space-separated elements for b{z}: ").split()]
-    # Append the completed list (row) to your main matrices
-    a.append(row_a)
-    b.append(row_b)
-
+a = [int(x) for x in input("Enter elements for a: ").split()]
+b = [int(x) for x in input("Enter elements for b: ").split()]
 
 if a[0:]<b[0:]:
     print(-1)
