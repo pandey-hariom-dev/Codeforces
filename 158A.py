8a7da4588599edee73bdaf22c792918b3d6bd72e
pandey-hariom-dev <pandey.hariom.dev@gmail.com>
2026-06-28 15:15:40 +0000

    Jun 28, 2026, 8:45 PM

--- 158A.py
+++ 158A.py
@@ -1,16 +1,32 @@
-n, c = input().split(" ")
-n, c = int(n), int(c)
+t = int(input())
 
-a = []
-b = []
-a = [int(x) for x in input("Enter elements for a: ").split()]
-b = [int(x) for x in input("Enter elements for b: ").split()]
+for _ in range(t):
+    n, c = map(int, input().split())
 
-time = 0
-if a[0:]<b[0:]:
-    print(-1)
-else:
-    while (a!=b):
-        a.sort()
-        time+=1
+    a = [int(x) for x in input().split()]
+    b = [int(x) for x in input().split()]
+
+    cost_without_reorder = 0
+    possible_without_reorder = True
+    for i in range(n):
+        if a[i] < b[i]:
+            possible_without_reorder = False
+            break
+        cost_without_reorder += (a[i] - b[i])
         
+    a_sorted = sorted(a)
+    b_sorted = sorted(b)
+    
+    possible_with_reorder = True
+    for i in range(n):
+        if a_sorted[i] < b_sorted[i]:
+            possible_with_reorder = False
+            break
+        cost_with_reorder += (a_sorted[i] - b_sorted[i])
+        
+    if not possible_with_reorder:
+        print(-1)
+    elif not possible_without_reorder:
+        print(cost_with_reorder)
+    else:
+        print(min(cost_without_reorder, cost_with_reorder))
