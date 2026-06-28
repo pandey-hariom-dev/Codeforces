fbbbfc2d01ea40d8ff4e9745f8ac9d0f567c6609
pandey-hariom-dev <pandey.hariom.dev@gmail.com>
2026-06-28 15:10:50 +0000

    Jun 28, 2026, 8:40 PM

--- 158A.py
+++ 158A.py
@@ -6,5 +6,11 @@ b = []
 a = [int(x) for x in input("Enter elements for a: ").split()]
 b = [int(x) for x in input("Enter elements for b: ").split()]
 
+time = 0
 if a[0:]<b[0:]:
     print(-1)
+else:
+    while (a!=b):
+        a.sort()
+        time+=1
+        
